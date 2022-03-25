9f8b7934ccfcefebafd7dc5b6e8bfc990806302e
Ken <kenblackuoo@gmail.com>
2022-03-25 15:31:23 -0400

    Created the Human class

--- Player.cs
+++ Player.cs
@@ -5,12 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace RPSLS
+
 {
     public class Player
     {
         public string name;
         public string gesterSelected;
         public int wins;
-        public string[]gestureList = {'rock', 'paper', 'scissors','lizard', 'spock'}
+        public string [] gestureList = {'rock', 'paper', 'scissors','lizard', 'spock'}
     }
 }
