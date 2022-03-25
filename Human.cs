f1c950f59516e46af124e644e237bffbe4267e2e
Ken <kenblackuoo@gmail.com>
2022-03-25 14:42:19 -0400

    Created two classes for this project

---
+++ Human.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Human
+{
+	public Human()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
---
+++ Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public class Player
+    {
+        public string name;
+        public string gesterSelected;
+        public int wins;
+        public string[]gestureList = {'rock', 'paper', 'scissors','lizard', 'spock'}
+    }
+}
