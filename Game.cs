57380d677b2e038c69badc331c6b1e507d7a3bcb
Ken <kenblackuoo@gmail.com>
2022-03-29 16:22:07 -0400

    Added displaywinner functions

--- Game.cs
+++ Game.cs
@@ -57,6 +57,7 @@ namespace RPSLS
 						AI playerTwo = new AI();
 						playerOne.setName();
 						playerTwo.setName();
+						aiGameRound(playerOne, playerTwo);
 						gameType = true;
 					}
 					else if (typeOfGame == 2)
@@ -159,6 +160,114 @@ namespace RPSLS
 					Console.WriteLine($"{ playerTwo.name} won this round by having {playerTwo.gestureSelected} over {playerOne.name}'s {playerOne.gestureSelected}");
 
 				}
+			}
+			huDisplayWinner(playerOne, playerTwo);
+		}
+		public void aiGameRound(Human playerOne, AI playerTwo)
+		{
+			int roundCount = 1;
+
+			while (playerOne.wins < 2 && playerTwo.wins < 2)
+			{
+				if (playerOne.gestureSelected == playerTwo.gestureSelected)
+				{
+					Console.WriteLine($"This round is a tie you both had {playerTwo.gestureSelected}");
+				}
+				else if (playerOne.gestureSelected == playerOne.gestureList[0] && playerTwo.gestureSelected == playerTwo.gestureList[2])
+				{
+					playerOne.wins += 1;
+					roundCount += 1;
+					Console.WriteLine($"{ playerOne.name} won this round by having {playerOne.gestureList[0]} over {playerTwo.name}'s {playerTwo.gestureList[2]}");
+				}
+				else if (playerOne.gestureSelected == playerOne.gestureList[0] && playerTwo.gestureSelected == playerTwo.gestureList[3])
+				{
+					playerOne.wins += 1;
+					roundCount += 1;
+					Console.WriteLine($"{ playerOne.name} won this round by having {playerOne.gestureList[0]} over {playerTwo.name}'s {playerTwo.gestureList[3]}");
+				}
+				else if (playerOne.gestureSelected == playerOne.gestureList[2] && playerTwo.gestureSelected == playerTwo.gestureList[1])
+				{
+					playerOne.wins += 1;
+					roundCount += 1;
+					Console.WriteLine($"{ playerOne.name} won this round by having {playerOne.gestureList[2]} over {playerTwo.name}'s {playerTwo.gestureList[1]}");
+				}
+				else if (playerOne.gestureSelected == playerOne.gestureList[2] && playerTwo.gestureSelected == playerTwo.gestureList[3])
+				{
+					playerOne.wins += 1;
+					roundCount += 1;
+					Console.WriteLine($"{ playerOne.name} won this round by having {playerOne.gestureList[2]} over {playerTwo.name}'s {playerTwo.gestureList[3]}");
+				}
+				else if (playerOne.gestureSelected == playerOne.gestureList[1] && playerTwo.gestureSelected == playerTwo.gestureList[0])
+				{
+					playerOne.wins += 1;
+					roundCount += 1;
+					Console.WriteLine($"{ playerOne.name} won this round by having {playerOne.gestureList[1]} over {playerTwo.name}'s {playerTwo.gestureList[0]}");
+				}
+				else if (playerOne.gestureSelected == playerOne.gestureList[1] && playerTwo.gestureSelected == playerTwo.gestureList[4])
+				{
+					playerOne.wins += 1;
+					roundCount += 1;
+					Console.WriteLine($"{ playerOne.name} won this round by having {playerOne.gestureList[1]} over {playerTwo.name}'s {playerTwo.gestureList[4]}");
+				}
+				else if (playerOne.gestureSelected == playerOne.gestureList[3] && playerTwo.gestureSelected == playerTwo.gestureList[4])
+				{
+					playerOne.wins += 1;
+					roundCount += 1;
+					Console.WriteLine($"{ playerOne.name} won this round by having {playerOne.gestureList[3]} over {playerTwo.name}'s {playerTwo.gestureList[4]}");
+				}
+				else if (playerOne.gestureSelected == playerOne.gestureList[3] && playerTwo.gestureSelected == playerTwo.gestureList[1])
+				{
+					playerOne.wins += 1;
+					roundCount += 1;
+					Console.WriteLine($"{ playerOne.name} won this round by having {playerOne.gestureList[3]} over {playerTwo.name}'s {playerTwo.gestureList[1]}");
+				}
+				else if (playerOne.gestureSelected == playerOne.gestureList[4] && playerTwo.gestureSelected == playerTwo.gestureList[2])
+				{
+					playerOne.wins += 1;
+					roundCount += 1;
+					Console.WriteLine($"{ playerOne.name} won this round by having {playerOne.gestureList[4]} over {playerTwo.name}'s {playerTwo.gestureList[2]}");
+				}
+				else if (playerOne.gestureSelected == playerOne.gestureList[4] && playerTwo.gestureSelected == playerTwo.gestureList[0])
+				{
+					playerOne.wins += 1;
+					roundCount += 1;
+					Console.WriteLine($"{ playerOne.name} won this round by having {playerOne.gestureList[4]} over {playerTwo.name}'s {playerTwo.gestureList[0]}");
+				}
+				else
+				{
+					roundCount += 1;
+					playerTwo.wins += 1;
+					Console.WriteLine($"{ playerTwo.name} won this round by having {playerTwo.gestureSelected} over {playerOne.name}'s {playerOne.gestureSelected}");
+
+				}
+			}
+			aiDisplayWinner(playerOne, playerTwo);
+		}
+
+		public void huDisplayWinner(Human playerOne, Human playerTwo)
+        {
+			if (playerOne.wins == 2)
+            {
+				Console.WriteLine($"{playerOne.name} wins the game!");
+				Console.WriteLine($"Summary of the game: The Winner {playerOne.name} had {playerOne.wins} || The Loser {playerTwo.name} had {playerTwo.wins}");
+            }
+			else if (playerTwo.wins == 2)
+            {
+				Console.WriteLine($"{playerTwo.name} wins the game!");
+				Console.WriteLine($"Summary of the game: The Winner {playerTwo.name} had {playerTwo.wins} || The Loser {playerOne.name} had {playerOne.wins}");
+			}
+        }
+		public void aiDisplayWinner(Human playerOne, AI playerTwo)
+		{
+			if (playerOne.wins == 2)
+			{
+				Console.WriteLine($"{playerOne.name} wins the game!");
+				Console.WriteLine($"Summary of the game: The Winner {playerOne.name} had {playerOne.wins} || The Loser {playerTwo.name} had {playerTwo.wins}");
+			}
+			else if (playerTwo.wins == 2)
+			{
+				Console.WriteLine($"{playerTwo.name} wins the game!");
+				Console.WriteLine($"Summary of the game: The Winner {playerTwo.name} had {playerTwo.wins} || The Loser {playerOne.name} had {playerOne.wins}");
 			}
 		}
 	}
